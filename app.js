55b183069c3616fc98f6abdaae3882e59719bb93
Kazniyaz Kabyldenov <kazniyazkz@gmail.com>
2018-07-22 15:53:09 -0400

    solution to the wnews

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
-const express = require("express");
-const morgan = require("morgan");
-const postBank = require("./postBank");
-const postList = require("./views/postList");
-const postDetails = require("./views/postDetails");
+const express = require('express');
+const morgan = require('morgan');
+//const postBank = require('./postBank');
+const postList = require('./views/postList');
+const postDetails = require('./views/postDetails');
+const client = require('./db/index');
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
-  const posts = postBank.list();
-  res.send(postList(posts));
+app.get('/', async (req, res, next) => {
+  try {
+    const data = await client.query(`SELECT *, counting.upvotes
+    FROM posts
+    JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting
+    ON posts.id = counting.postId;`);
+    const posts = data.rows;
+    res.send(postList(posts));
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get("/posts/:id", (req, res) => {
-  const post = postBank.find(req.params.id);
-  res.send(postDetails(post));
+app.get('/posts/:id', async (req, res, next) => {
+  try {
+    var post = await client.query(
+      `SELECT * FROM posts where userid=${req.params.id}`
+    );
+    post = post.rows;
+    res.send(postDetails(post[0]));
+  } catch (error) {
+    next(error);
+  }
 });
 
 const PORT = 1337;
